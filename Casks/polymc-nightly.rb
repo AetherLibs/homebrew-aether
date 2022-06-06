5e049bd66cc55173802c94486c5f7aaaf2817a91
Ethan <git@ethanlibs.co>
2022-06-05 20:30:00 -0400

    Create polymc-nightly.rb

---
+++ Casks/polymc-nightly.rb
@@ -0,0 +1,21 @@
+cask "polymc" do
+  version "1.3.1"
+  sha256 "847345bb33c4b454c383a4f3e4f051b08433fc2839ef1446cac3a109fb3bf8aa"
+
+  url "https://github.com/PolyMC/PolyMC/releases/download/#{version}/PolyMC-macOS-#{version}.tar.gz",
+      verified: "github.com/PolyMC/PolyMC/"
+  name "PolyMC"
+  desc "Minecraft launcher"
+  homepage "https://polymc.org/"
+
+  depends_on macos: ">= :high_sierra"
+
+  app "PolyMC.app"
+
+  zap trash: [
+    "~/Library/Application Support/PolyMC/metacache",
+    "~/Library/Application Support/PolyMC/PolyMC-*.log",
+    "~/Library/Application Support/PolyMC/polymc.cfg",
+    "~/Library/Saved Application State/org.polymc.PolyMC.savedState",
+  ]
+end
