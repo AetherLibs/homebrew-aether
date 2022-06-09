a7e8329f785b8cb446b3131991bff076e51ac4b4
Ethan <git@ethanlibs.co>
2022-06-09 09:55:32 -0400

    discord-canary 0.0.284 (new cask)
    * Create discord-canary.rb
    * Update discord-canary.rb
    * Update discord-canary.rb
    * Update discord-canary.rb
    * Update discord-canary.rb
    
    Closes #3.
    
    Signed-off-by: Ethan <git@ethanlibs.co>

---
+++ Casks/discord-canary.rb
@@ -0,0 +1,29 @@
+cask "discord-canary" do
+  version "0.0.284"
+  sha256 "adf1d5ffd900aa7df1227c1961c060ce202a370fbabd4f0548ebcd714c7c27f1"
+
+  url "https://dl-canary.discordapp.net/apps/osx/#{version}/DiscordCanary.dmg",
+      verified: "dl-canary.discordapp.net/"
+  name "Discord Canary"
+  desc "Voice and text chat software"
+  homepage "https://canary.discord.com/"
+
+  livecheck do
+    url "https://discord.com/api/download/canary?platform=osx"
+    strategy :header_match
+  end
+
+  auto_updates true
+
+  app "Discord Canary.app"
+
+  zap trash: [
+    "~/Library/Application Support/discord",
+    "~/Library/Caches/com.hnc.Discord",
+    "~/Library/Caches/com.hnc.Discord.ShipIt",
+    "~/Library/Cookies/com.hnc.Discord.binarycookies",
+    "~/Library/Preferences/com.hnc.Discord.helper.plist",
+    "~/Library/Preferences/com.hnc.Discord.plist",
+    "~/Library/Saved Application State/com.hnc.Discord.savedState",
+  ]
+end
